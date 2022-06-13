0b28d0919d1f282847f28dd86780c277e688d4c4
Winson Quach <winson.skole@gmail.com>
2022-06-13 12:56:31 +0200

    Fully Finished Repository Test For Location Database

--- API/API_Test/LocationsTest/RepositoryTest_Location.cs
+++ API/API_Test/LocationsTest/RepositoryTest_Location.cs
@@ -59,5 +59,111 @@
             IsType<Locations>(result);
             Equal(1, result.ID);
         }
+
+        [Fact]
+        public async Task Get_By_ID_Should_Return_Null_When_No_Location_Found()
+        {
+            await _context.Database.EnsureDeletedAsync();
+
+            var result = await _sut.GetByID(1);
+            Null(result);
+        }
+
+        [Fact]
+        public async Task Create_New_Location_Should_Add_New_Location_To_Database()
+        {
+            Intel();
+
+            Locations newLocation = new Locations
+            {
+                Postal = 3333,
+                City = "City Name 3 Here"
+            };
+            await _context.SaveChangesAsync();
+
+            var result = await _sut.AddNewLocation(newLocation);
+
+            NotNull(result);
+            IsType<Locations>(result);
+            Equal(3, result.ID);
+        }
+
+        [Fact]
+        public async Task Create_New_Location_Should_Return_Error_When_Already_Exists()
+        {
+            await _context.Database.EnsureDeletedAsync();
+            Locations ExistingLocation = new Locations
+            {
+                ID = 1,
+                Postal = 1111,
+                City = "City Name 1 Here"
+            };
+            _context.Locations.Add(ExistingLocation);
+            await _context.SaveChangesAsync();
+
+            Func<Task> action = async () => await _sut.AddNewLocation(ExistingLocation);
+
+            var result = await ThrowsAsync<ArgumentException>(action);
+            Contains("An item with the same key has already been added", result.Message);
+        }
+
+        [Fact]
+        public async Task Update_Location_Should_Update_Location_Data_With_New_Values()
+        {
+            Intel();
+            Locations UpdatedLocation = new Locations
+            {
+                ID = 1,
+                Postal = 1234,
+                City = "New City Name 1 Here"
+            };
+
+            var result = await _sut.UpdateLocation(1, UpdatedLocation);
+
+            NotNull(result);
+            IsType<Locations>(result);
+            Equal(UpdatedLocation.Postal, result.Postal);
+            Equal(UpdatedLocation.City, result.City);
+        }
+
+        [Fact]
+        public async Task Update_Location_Should_Return_Error_When_Existing_Location_Not_Found()
+        {
+            await _context.Database.EnsureDeletedAsync();
+
+            Locations UpdatedLocation = new Locations
+            {
+                ID = 1,
+                Postal = 1111,
+                City = "Return error Not Found"
+            };
+            var result = await _sut.UpdateLocation(1, UpdatedLocation);
+
+            Null(result);
+        }
+
+        [Fact]
+        public async Task Delete_Location_Should_Remove_Location_From_Database()
+        {
+            Intel();
+
+            var result = await _sut.DeleteLocation(1);
+            var usedToEmptyListPleaseIgnore = await _sut.DeleteLocation(2);
+            var list = await _sut.GetAll();
+
+            NotNull(result);
+            IsType<Locations>(result);
+            Equal(1, result.ID);
+                
+            Empty(list);
+        }
+
+        [Fact]
+        public async Task Delete_Location_Should_Return_Error_When_Location_Not_Found()
+        {
+            await _context.Database.EnsureDeletedAsync();
+            var result = await _sut.DeleteLocation(1);
+            Null(result);
+        }
     }
 }
